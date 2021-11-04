144cb8970c51801ebc1fed1b83a6d59ff2368cff
AD\arebiere2018 <alexis.rebiere2018@campus-eni.fr>
2021-11-04 16:06:17 +0100

    Commit Beans + RouteUtils + TestPokemonBeans

--- src/main/java/beans/Pokemon.java
+++ src/main/java/beans/Pokemon.java
@@ -11,10 +11,14 @@ public class Pokemon {
 	private String elemType;
 	private Ability ability;
 	
+	private static double STRONG_VS = 1.5;
+	private static double WEAK_VS = 0.5;
+	private static String WATER_TYPE = "Eau";
+	private static String FIRE_TYPE = "Feu";
+	private static String PLANT_TYPE = "Plante";
 	
 	
-	
-	public Pokemon() {
+ 	public Pokemon() {
 		super();
 	}
 
@@ -93,6 +97,55 @@ public class Pokemon {
 
 	public void setAbilty(Ability ability) {
 		this.ability = ability;
+	}
+		
+	@Override
+	public String toString() {
+		return "Pokemon [name=" + name + ", lifepoints=" + lifepoints + ", attack=" + attack + ", defense=" + defense
+				+ ", speed=" + speed + ", elemType=" + elemType + ", ability=" + ability.toString() + "]";
+	}
+
+	// This method initializes a fight between 2 Pokemon objects
+	public static String fight(Pokemon attacker, Pokemon defender) {
+		System.out.println("[Pokemon-Beans fight(" + 
+							attacker.toString() + "," + 
+							defender.toString() +")]");
+		String winner = "";
+		
+		// While... allows us to repeat the fight until one's lifepoints reach 0
+		while((attacker.getLifepoints() > 0) 
+				&& (defender.getLifepoints() > 0)) {
+			attacker.attack(defender);
+			defender.attack(attacker);
+			
+			// Testing whether attacker or defender have reached 0 lifepoints, in this case the concerned Pokemon has its LP set to 0
+			// and we affect to a local variable winner the winning Pokemon's name 
+			if(attacker.getLifepoints() <= 0) {
+				// Attacker dies and HP are set to 0
+				attacker.setLifepoints(0);
+				winner = defender.getName();
+				break;
+			}else if(defender.getLifepoints() <= 0) {
+				// Defender dies and HP are set to 0
+				defender.setLifepoints(0);
+				winner = attacker.getName();
+				break;
+			}
+		
+		}
+		return winner; 
+	
+	}
+
+	// This method allows a Pokemon to reduce a defender Pokemon's life points, depending on both statistics
+	private void attack(Pokemon defender) {
+		System.out.println("[Pokemon-Beans attacker.attack(" + 
+				defender.toString() +")]");
+		double damageCalculation = this.attack * ((this.ability.getPower())/10);
+		double damageReduction = (defender.defense % 5) / 100;
+		double actualLifePoints = this.lifepoints;
+		defender.setLifepoints((int)( actualLifePoints - (damageCalculation - damageReduction)));
+		
 	}
 	
 	
--- src/main/java/utils/RouteUtils.java
+++ src/main/java/utils/RouteUtils.java
@@ -3,4 +3,5 @@ package utils;
 public class RouteUtils {
 
 	public static final String ROUTE_HOME = "/WEB-INF/jsp/home.jsp";
+	public static final String ROUTE_POKEMON_TEST = "/WEB-INF/jsp/pokemon-test.jsp";
 }
